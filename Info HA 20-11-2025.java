19e1d149326ef797dd58f452e28b7bc407495e5d
Schinkennugget <168964051+Schinkennugget@users.noreply.github.com>
2025-11-20 13:03:19 +0100

    Add Knoten and GraphMatrix classes
    Added classes Knoten and GraphMatrix with initial structure.

--- Info HA 20-11-2025.java
+++ Info HA 20-11-2025.java
@@ -1,1 +1,12 @@
+import java.util.*;
+
+class Knoten {
+  private String bezeichner;
+  Knoten(String bezeichner) {
+    this.bezeichner = bezeichner;
+  }
+}
+
+class GraphMatrix {
+  ArrayList<ArrayList<Integer>> matrix = new ArrayList<ArrayList<Integer>>;
 
